fce3bb1a53720cffb139dd2efc94cbbd35403d3f
Boon Tek <darkon.garon@gmail.com>
2018-10-26 12:30:28 +0800

    1. before attempting to fix user, duty and reservation models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "welcome#index"
+
+  resources :users, only:[:create, :edit, :show, :update]
+
+  get "/sign_up" => "users#new", as: "sign_up"
+  get "/sign_in" => "sessions#new", as: "sign_in"
 end
