0ac6132d778f870ab1f7713e9b783492479fd4b2
Boon Tek <darkon.garon@gmail.com>
2018-10-28 10:53:32 +0800

    1. edited migration file 'create_user' to not be harsh on ic_number, sjam_id, division, and phone_number

--- db/migrate/20181025072349_create_users.rb
+++ db/migrate/20181025072349_create_users.rb
@@ -5,12 +5,12 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email, null: false
       t.string :password_digest, limit: 128, null: false
       t.string :name, null: false
-      t.string :ic_number, null: false
-      t.string :sjam_id, null: false
-      t.string :division, null: false
+      t.string :ic_number
+      t.string :sjam_id
+      t.string :division
       t.string :phone_number
       t.boolean :driver_status, default: false
-      t.string :role # Member, CVSC, PDC, Administrator
+      t.string :role, default: "Member" # Member, CVSC, PDC, Administrator
     end
   end
 end
