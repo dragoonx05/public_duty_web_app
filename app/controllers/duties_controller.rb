65795f504b9844c75a21fd79bca9ce59be7f0e09
Boon Tek <darkon.garon@gmail.com>
2018-10-28 12:54:29 +0800

    1. users can create duties

--- app/controllers/duties_controller.rb
+++ app/controllers/duties_controller.rb
@@ -2,4 +2,32 @@ class DutiesController < ApplicationController
 
     before_action :authorize
     
+    def new
+        @duty = Duty.new
+    end
+
+    def create
+        duty = Duty.new(new_duty_params)
+        duty.user_id = current_user.id
+        if duty.save
+            flash[:notice] = "Duty successfully registered!"
+            redirect_to duties_path
+        else
+            flash[:alert] = "Unable to register duty, check details and retry!"
+            render :new
+        end 
+    end
+
+    def edit
+    end
+
+    def delete
+    end
+
+    private
+    
+    def new_duty_params
+        params.require(:duty).permit(:duty_name, :venue, :start_date_time, :end_date_time, :contact_person, :contact_number)
+    end
+
 end
