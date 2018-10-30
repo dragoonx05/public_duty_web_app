eb373e723cdf13201734e6198da59205fedf5c36
Boon Tek <darkon.garon@gmail.com>
2018-10-30 14:11:02 +0800

    1. fixed user_flow_spec

--- spec/feature/user_flow_spec.rb
+++ spec/feature/user_flow_spec.rb
@@ -13,9 +13,9 @@ RSpec.feature "New", :type => :feature do
   # edgy path
   scenario "User creates a new account with invalid email" do
     visit "/sign_up"
-    fill_in "user[name]", :with => "Jolynn"
-    fill_in "user[email]", :with => "gmail.com"
-    fill_in "user[password]", :with => "11111111"
+    fill_in "users[name]", :with => "Jolynn"
+    fill_in "users[email]", :with => "gmail.com"
+    fill_in "users[password]", :with => "11111111"
     click_button "Save Users"
     expect(page).to have_text("User Sign up Page")
   end
